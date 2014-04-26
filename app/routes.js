1dda8b0c102f27845fd24ad6eb88cb7e6738a1f2
Christopher Su <christophersu9@gmail.com>
2014-04-26 10:19:53 -0700

    fixed index

--- app/routes.js
+++ app/routes.js
@@ -3,9 +3,9 @@ module.exports = function(app, passport) {
 // normal routes ===============================================================
 
 	// show the home page (will also have our login links)
-	app.get('/', function(req, res) {
-		res.render('index.ejs');
-	});
+	// app.get('/', function(req, res) {
+	// 	res.render('index.ejs');
+	// });
 
 	// PROFILE SECTION =========================
 	app.get('/profile', isLoggedIn, function(req, res) {
