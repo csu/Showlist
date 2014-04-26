2bad6b7943e0fe5c6bed21167c26d0756c7e76b0
Christopher Su <christophersu9@gmail.com>
2014-04-26 04:21:20 -0700

    problems with user reg

--- config/auth.js
+++ config/auth.js
@@ -5,6 +5,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' 		: '647376201977611', // your App ID
 		'clientSecret' 	: '4b3139b521b2881943929c8f0735bdaf', // your App Secret
-		'callbackURL' 	: 'http://dev.christophersu.net:8080/auth/facebook/callback'
+		'callbackURL' 	: 'http://dev.christophersu.net:3000/auth/facebook/callback'
 	}
 };
--- models/user.js
+++ models/user.js
@@ -1,16 +1,16 @@
 var restful = require('node-restful'),
     mongoose = require('mongoose');
 
-var User = new restful.Model({
-  title: 'user',
-  methods: ['get', 'post', 'put', 'delete'],
-  schema: mongoose.Schema({
-    provider: { type: 'string', required: true},
+var User = restful.model('user', mongoose.Schema({
+    provider: { type: 'string', required: true },
     user_id: { type: 'string', unique: true, required: true},
-    full_name: { type: 'string', required: true},
+    name: { type: 'string', required: true},
     email: { type: 'string', required: true},
-    photo: { type: 'string', required: true},
-  }),
-});
+    photo: { type: 'string'},
+    facebook_token: { type: 'string', required: true}
+  }))
+  .methods(['get', 'post', 'put', 'delete']);
 
-exports = module.exports = User;
+User.register(app, '/api/user');
+
+module.exports = mongoose.model('User', userSchema);
