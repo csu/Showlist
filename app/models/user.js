f767640c603de1f492f4ead7e0112a07c7a41d79
Christopher Su <christophersu9@gmail.com>
2014-04-26 05:00:09 -0700

    working now

--- app/models/user.js
+++ app/models/user.js
@@ -1,44 +1,54 @@
 // load the things we need
 var mongoose = require('mongoose');
-var bcrypt   = require('bcrypt-nodejs');
+// var bcrypt   = require('bcrypt-nodejs');
 
 // define the schema for our user model
-var userSchema = mongoose.Schema({
+// var userSchema = mongoose.Schema({
 
-    local            : {
-        email        : String,
-        password     : String,
-    },
-    facebook         : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
-    }
+//     local            : {
+//         email        : String,
+//         password     : String,
+//     },
+//     facebook         : {
+//         id           : String,
+//         token        : String,
+//         email        : String,
+//         name         : String
+//     },
+//     twitter          : {
+//         id           : String,
+//         token        : String,
+//         displayName  : String,
+//         username     : String
+//     },
+//     google           : {
+//         id           : String,
+//         token        : String,
+//         email        : String,
+//         name         : String
+//     }
 
-});
+// });
 
-// generating a hash
-userSchema.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
 
-// checking if password is valid
-userSchema.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.local.password);
-};
+var userSchema = new mongoose.Schema({
+    user_id: { type: String, unique: true, required: true},
+    name: { type: String, required: true},
+    email: { type: String, required: true},
+    photo: { type: String},
+    facebook_token: { type: String, required: true}
+  });
+
+
+// // generating a hash
+// userSchema.methods.generateHash = function(password) {
+//     return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+// };
+
+// // checking if password is valid
+// userSchema.methods.validPassword = function(password) {
+//     return bcrypt.compareSync(password, this.local.password);
+// };
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
