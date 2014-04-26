e01255e8cb02850c9171bf675a40f32518135689
Christopher Su <christophersu9@gmail.com>
2014-04-26 03:33:24 -0700

    now in express 3

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+	'url' : 'mongodb://MVqC8QD2rrvjkyZPjoTf:ENmZj4KRND8qHKmLTqxL@ds031747.mongolab.com:31747/fbhack2014-2' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
